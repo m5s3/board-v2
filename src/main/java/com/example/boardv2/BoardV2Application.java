9a31f96946a332294fc7c7f506d277103af3a2c5
m5s3 <devmyoungsoo2@gmail.com>
2023-05-08 13:36:30 +0900

    #5 - 스프링 부트 시작

---
+++ src/main/java/com/example/boardv2/BoardV2Application.java
@@ -0,0 +1,13 @@
+package com.example.boardv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardV2Application.class, args);
+    }
+
+}
