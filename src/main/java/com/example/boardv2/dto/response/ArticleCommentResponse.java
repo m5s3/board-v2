aab3d2a8162c6fbf952eacba2b33dfd3998fe6a6
m5s3 <devmyoungsoo2@gmail.com>
2023-05-30 13:11:34 +0900

    #49 - 대댓글 비즈니스 로직 구현
    부모/자식 관계를 만들면서 entity <-> dto 간 변환 로직,
    댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법,
    댓글과 대댓글을 db 데이터로부터 변환할 때
    서로 계층 구조를 만들고 각각 정렬하는 과정을 구현

--- src/main/java/com/example/boardv2/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/example/boardv2/dto/response/ArticleCommentResponse.java
@@ -3,17 +3,29 @@ package com.example.boardv2.dto.response;
 import com.example.boardv2.dto.ArticleCommentDto;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public record ArticleCommentResponse(
         Long id,
         String content,
         LocalDateTime createdAt,
-        String userId,
         String email,
-        String nickname
+        String nickname,
+        String userId,
+        Long parentCommentId,
+        Set<ArticleCommentResponse> childComments
 ) {
-    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt,String userId, String email, String nickname) {
-        return new ArticleCommentResponse(id, content, createdAt,userId, email, nickname);
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
+        //return new ArticleCommentResponse(id, content, createdAt,userId, email, nickname);
+        return ArticleCommentResponse.of(id, content, createdAt, userId, email, nickname, null);
+    }
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId, Long parentCommentId) {
+        Comparator<ArticleCommentResponse> childCommentComparator = Comparator
+                .comparing(ArticleCommentResponse::createdAt)
+                .thenComparingLong(ArticleCommentResponse::id);
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId, parentCommentId, new TreeSet<>(childCommentComparator));
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
@@ -22,13 +34,18 @@ public record ArticleCommentResponse(
             nickname = dto.userAccountDto().userId();
         }
 
-        return new ArticleCommentResponse(
+        return ArticleCommentResponse.of(
                 dto.id(),
                 dto.content(),
                 dto.createdAt(),
-                dto.userAccountDto().userId(),
                 dto.userAccountDto().email(),
-                nickname
+                nickname,
+                dto.userAccountDto().userId(),
+                dto.parentCommentId()
         );
+    }
+
+    public boolean hasParentComment() {
+        return parentCommentId != null;
     }
 }
